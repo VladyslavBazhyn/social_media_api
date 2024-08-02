a25958d80d05f96db2d6659fdb37bd29d6dd2cbc
Gibbs <mr.darmstadtiumgmail.com>
2024-08-02 15:20:16 +0200

    Fix tupos in User model

--- social_media_user/models.py
+++ social_media_user/models.py
@@ -33,13 +33,13 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create and save a regular User with the given email and password."""
-        extra_fields.setdefault("is_stuff", False)
+        extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
         """Create and save a SuperUser with the given email and password."""
-        extra_fields.setdefault("is_stuff", True)
+        extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
 
         if extra_fields.get("is_staff") is not True:
@@ -52,14 +52,14 @@ class UserManager(BaseUserManager):
 
 class User(AbstractUser):
 
-    objects = UserManager
+    objects = UserManager()
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     username = None
     email = models.EmailField(_("email address"), unique=True)
     bio = models.TextField(default="Any biography here")
-    birth_date = models.DateField()
+    birth_date = models.DateField(null=True, blank=True)
     image = models.ImageField(upload_to=user_image_file_path, blank=True, null=True)
     my_subscriptions = models.ManyToManyField(
         "self",
